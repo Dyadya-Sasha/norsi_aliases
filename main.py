c713fed234842c86b443dd7ce159d9772435d048
Dyadya-Sasha <constantsy@gmail.com>
2022-12-15 19:52:55 +0300

    added pattern for IP and port

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-# This is a sample Python script.
-
-from __future__ import with_statement
 import re
+import os
 from time import sleep
 import subprocess
 import sys
+import socket
+import argparse
 
-names = []
-cmd = []
-fin_dict = {}
 pattern_name = r'\b[A-Z].*(?==)'
 pattern_command = r'\bssh\s.*(?=\")'
-global_iterator = 0
-pattern_ip = r'[\d{}]{,3}(?:[.][\d{}]{,3}){3}'
+pattern_ip = r'[\d]{,3}(?:[.][\d]{,3}){3}'
 pattern_port = r'\d{4}'
+unicode_status = "\u25C9"
+united_dict = {}
 
 
 class RGB:
@@ -29,75 +27,112 @@ def color_text(text, rgb):
 
 
 def finder(pat, text):
-    match = re.search(pat, str(text))
-    str_match = format(match.group(0))
-    return str_match
+    try:
+        match_ = re.search(pat, str(text))
+        str_match = format(match_.group(0))
+        return str_match
+    except AttributeError:
+        print("Something is wrong with reference file")
+        sys.exit()
 
 
 def parser():
+    order = []
+    names = []
+    cmd = []
+    ip_list = []
+    port_list = []
+    port_avail = []
+    global united_dict
     with open('norsi_aliases', 'r') as f:
         test = f.readlines()
         i = 0
         for line in test:
-            #            print(color_text(finder(pattern_name, line), RGB.RED))
-            #            print(color_text(finder(pattern_command, line), RGB.GREEN))
-            names.append(finder(pattern_name, line))
-            cmd.append(finder(pattern_command, line))
-            i = i + 1
+            if line == "\n":
+                continue
+            else:
+                order.append(i)
+                names.append(finder(pattern_name, line))
+                cmd.append(finder(pattern_command, line))
+                ip_list.append(finder(pattern_ip, line))
+                port_list.append(finder(pattern_port, line))
+                port_avail.append(port_test(finder(pattern_ip, line), finder(pattern_port, line)))
+                i = i + 1
     f.close()
-
-
-# def print_list(listik):
-#     for i in range(len(listik)):
-#         print(listik[i])
+    united_dict = {z[0]: list(z[1:]) for z in zip(order, names, cmd, ip_list, port_list, port_avail)}
+    names.clear()
+    cmd.clear()
+    ip_list.clear()
+    port_list.clear()
+    order.clear()
 
 
 def ssh_connect(name, command):
-    # hostname = "192.168.122.248"
-    # username = "root"
-    # password = "qwedsa"
-    # s = paramiko.SSHClient()
-    # s.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    # s.connect(hostname="192.168.122.248", username="root", password="qwedsa")
-    # while True:
-    #     try:
-    #         cmd = input("root>")
-    #         if cmd == "exit": break
-    #         stdin, stdout, stderr = s.exec_command(cmd)
-    #         print(stdout.read().decode())
-    #     except KeyboardInterrupt:
-    #         break
-    # s.close()
-    # subprocess.call('clear')
     print(f"\nConnecting to {name} {command}")
     try:
         subprocess.check_call(command, shell=True)
     except subprocess.CalledProcessError as e:
         print(e.output)
-        sleep(3)
+        sleep(0.8)
+
+
+def port_test(address, port):
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    a_socket.settimeout(0.2)
+    try:
+        result_of_check = a_socket.connect_ex((address, int(port)))
+        if result_of_check == 0:
+            # print("Port is open")
+            a_socket.close()
+            # print(f"{address}:{port} is online")
+            return RGB.GREEN
+        else:
+            # print("Port is not open")
+            a_socket.close()
+            # print(f"{address}:{port} is offline")
+            return RGB.RED
+    except socket.error:
+        return
 
 
 if __name__ == "__main__":
-    parser()
+    arg_parser = argparse.ArgumentParser(description="You can use this script in 2 ways - either direct SSH connection or grab information about nodes."
+                                                     "To grab info about nodes, you have to specify -t option")
+    arg_parser.add_argument("-t",
+                            type=str,
+                            help="Grab info about nodes")
+    option_keys = arg_parser.parse_args()
+    print(option_keys.t)
+    if option_keys.t == "-t":
+        sys.exit("-t option is enabled")
+
+    os.chdir(sys.path[0])
+    counter = 0
     while True:
+        x = 0
+        parser()
         try:
             subprocess.call('clear')
-            #    print_list(names)
-            #    print_list(cmd)
-            global_iterator = 1
-            for x in range(len(names)):
-                print("{}) {}".format(global_iterator, color_text(names[x], RGB.RED)))
-                print("    {}".format(color_text(cmd[x], RGB.GREEN)))
-                global_iterator += 1
+            for key, val in united_dict.items():
+                x += 1
+                print(f"{x :<2}) {color_text(unicode_status, val[4])} {color_text(val[0], RGB.RED)}")
+                print(f"      {color_text(val[1], RGB.GREEN)}")
+            # for key, val in united_dict.items():
+            # print(f"{key:}\n {val}")
             try:
-                inp = int(input("\nChoose your way, passer-by (any other key for exit):  "))
+                inp = input("\nChoose your destiny (any other key to exit):  ")
+                if inp.isdigit():
+                    inp = int(inp)
+                else:
+                    sys.exit('Exit')
             except ValueError:
                 sys.exit("Exit")
 
-            if 1 <= inp < 25:
-                ssh_connect(names[inp - 1], cmd[inp - 1])
+            if 1 <= inp <= len(united_dict):
+                ssh_connect(united_dict.get((inp - 1))[0], {united_dict.get((inp - 1))[1]})
+                # sleep(10)
             else:
                 print("Your choice is out range")
                 sleep(1)
         except KeyboardInterrupt:
-            print("\nInterrupted from keyboard")
+            pass
